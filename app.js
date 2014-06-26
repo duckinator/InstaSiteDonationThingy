2cd22cd297d54bae27f4dd0aa42034f7de9e58ea
Marie Markwell <me@marie.so>
2014-06-26 04:01:59 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// To make all of the 
+function template(id) {
+  return document.getElementById(id + "-template").innerHTML;
+}
+
+var instaSiteDonationThingy = angular.module('instaSiteDonationThingy', []);
+
+instaSiteDonationThingy
+  /*
+   * Usage:
+   *    donationConfig(function (config) {l
+   *      // ...
+   *    });
+   */
+  .factory('donationConfig', ['$http',
+    function ($http) {
+      return $http.get('config.json').success;
+    }])
+
+  .factory('venmoDetails', ['$http',
+    function ($http) {
+      // FIXME: Venmo's documentation says that an access token is required,
+      //        but it doesn't *appear* to be required right now.
+      //        Odds are, this will begin to fail at some point.
+      //        I've reached out to them on Twitter (https://twitter.com/duckinator/status/482046490060529664)
+      //        in hopes of them adding a way to embed a Venmo button
+      //        based directly on username (instead of doing a lookup).
+      function venmoDetails(username, callback) {
+        return $http.get('https://api.venmo.com/v1/users/' + username).success(callback);
+      }
+
+      return venmoDetails;
+    }])
+
+
+  /* ================================ PayPal ================================ */
+  .controller('PayPalWidgetCtrl', ['$scope', 'donationConfig',
+    function ($scope, donationConfig) {
+      donationConfig(function (config) {
+        $scope.email = config.paypal.email;
+      });
+    }])
+
+  .directive('paypalWidget',
+    function () {
+      return {
+        template: template('paypal'),
+        controller: 'PayPalWidgetCtrl'
+      };
+    })
+
+  /* ================================ Venmo  ================================ */
+  .controller('VenmoWidgetCtrl', ['$scope', 'donationConfig',
+    function ($scope, donationConfig) {
+      donationConfig(function (config) {
+        $scope.username = config.venmo.username;
+      });
+    }])
+
+    .directive('venmoWidget',
+      function () {
+        return {
+          template: template('venmo'),
+          controller: 'VenmoWidgetCtrl'
+        };
+      })
+  ;
